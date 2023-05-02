d2c33f42de23aed350ad79f5958a4197842e8302
jungbin-lee <hihihi486@naver.com>
2023-05-02 19:52:50 +0900

    Initial commit

---
+++ app/src/main/java/com/h2square/kotlinbasic/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.h2square.kotlinbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        clickBtn.setOnClickListener {
+    Log.d( "메인화면로그","클릭용 버튼 눌림")
+            Log.e("메인화면로그","에러임")
+        }
+        smallBtn.setOnClickListener {
+            Toast.makeText(this, "작은 버튼이 눌림", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
