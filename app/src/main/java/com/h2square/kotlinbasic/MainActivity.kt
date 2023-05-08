b6e6d6deceaa809890f560470b9152a01baab701
jungbin-lee <hihihi486@naver.com>
2023-05-08 15:37:31 +0900

    kotlinprac

--- app/src/main/java/com/h2square/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/h2square/kotlinbasic/MainActivity.kt
@@ -10,6 +10,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //문법연습용
+        val myName : String //value
+        var myAge : Int //variable
+        myName ="이정빈"
+
+        var haetae ="ddddd"
+        val ssss= 1
+        val ssssww= "sss"
+        myAge=12
+        myAge=2
+        println(myAge)
+    Log.d("변수", myName)
+//조건문
+        val usrAge =15
+        if(usrAge >= 20){
+            Log.d("성인 판별","성인입니다")
+        }
+        else if (usrAge>=17){
+            Log.d("성인 판별","고딩입니다.")
+        }  else if (usrAge>=14){
+            Log.d("성인 판별","중딩입니다.")
+        }
+        else{
+            Log.d("성인 ㅠㅏㄴ병","성인아닙니다.")
+        }
+val userAge =25
+        if (usrAge>=20){
+            Log.d("성인판별","성인입니다")
+        }
+        else{
+            Log.d("성인판별","tjddlsdkslqslek")
+        }
         clickBtn.setOnClickListener {
     Log.d( "메인화면로그","클릭용 버튼 눌림")
             Log.e("메인화면로그","에러임")
